f1de458511881cb2911eb0e78c18a1634a7659d5
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-06-17 01:36:10 -0300

    comentarios"

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -2,11 +2,15 @@
 
 use app\library\Router;
 
+// Carrega as dependências
 require '../vendor/autoload.php';
 
+// Inicia a sessão
 session_start();
 
+// Carrega as variáveis de ambiente (usando o pacote Dotenv)
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 2));
 $dotenv->load();
 
+// Cria uma instância do Router (roteador)
 $route = new Router;
