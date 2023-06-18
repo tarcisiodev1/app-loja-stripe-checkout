c71faf2e3abbddd8308eadff679833f560926033
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-06-18 02:42:00 -0300

    route

--- app/library/Route.php
+++ app/library/Route.php
@@ -4,23 +4,26 @@ namespace app\library;
 
 class Route
 {
-  public function __construct(
-    public string $uri,
-    public string $request,
-    public string $controller,
-  ) {
+  // Construtor da classe Route
+  public function __construct(public string $uri, public string $request, public string $controller)
+  {
   }
 
+  // Retorna a URI atual
   public function currentUri()
-  { //rtrim para tirar a ultima barra e o php url path tira a query string
+  {
+    // Utiliza a função parse_url para obter a URI atual
+    // A função rtrim remove a barra final da URL (caso exista)
     return $_SERVER['REQUEST_URI'] !== '/' ? rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/') : '/';
   }
 
+  // Retorna o método da requisição atual
   private function currentRequest()
   {
     return strtolower($_SERVER['REQUEST_METHOD']);
   }
 
+  // Verifica se a rota atual corresponde à rota definida
   public function match()
   {
     if (
