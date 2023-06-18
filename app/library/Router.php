68ba1eadef024c67c46dc4de7146a1f311172012
Tarcisio Santos Junior <tarcisiodev1@gmail.com>
2023-06-18 02:41:16 -0300

    router

--- app/library/Router.php
+++ app/library/Router.php
@@ -4,29 +4,28 @@ namespace app\library;
 
 class Router
 {
+  // Array para armazenar as rotas definidas
   private array $routes = [];
 
-  public function add(
-    string $uri,
-    string $request,
-    string $controller
-  ) {
-
+  // Adiciona uma nova rota ao array de rotas
+  public function add(string $uri, string $request, string $controller)
+  {
     $this->routes[] = new Route($uri, $request, $controller);
   }
 
+  // Inicializa o roteamento e direciona para o controlador adequado
   public function init()
   {
-
-
     foreach ($this->routes as $route) {
       if ($route->match($route)) {
-
+        // Registra o redirecionamento
         Redirect::register($route);
+        // Chama o método do controlador correspondente à rota
         return (new Controller)->call($route);
       }
     }
 
+    // Se nenhuma rota for encontrada, chama o controlador NotFoundController
     return (new Controller)->call(new Route('/404', 'GET', 'NotFoundController:index'));
   }
 }
